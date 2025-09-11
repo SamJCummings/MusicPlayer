98cf81ae89b3c3a6bf687891c90f9ceac64f7027
psysc <psysc10@nottingham.ac.uk>
2025-09-11 16:15:07 +0100

    create initial app state

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
-use cursive;
-use cursive::views::{Dialog, LinearLayout, ScrollView, SelectView};
-
 use std::path::{Path, PathBuf};
 use std::{env, fs, io};
+
+use cursive::traits::*;
+use cursive::views::{Dialog, DummyView, LinearLayout, ResizedView, ScrollView, SelectView};
+use cursive::{Cursive, CursiveRunnable};
 
 const MUSIC_FOLDER: &str = "documents/music";
 
 fn main() -> io::Result<()> {
     let mut app = create_app();
 
-    let mut list = SelectView::new();
-    list.add_all_str(load_files(None)?);
+    let layout = LinearLayout::horizontal()
+        .child(create_list_items("Artists", load_files(None)?))
+        .child(create_list_empty("Albums"))
+        .child(create_list_empty("Songs"))
+        .full_screen();
 
-    let layout =
-        LinearLayout::horizontal().child(Dialog::around(ScrollView::new(list)).title("Artists"));
-    app.add_layer(layout);
-
+    app.add_fullscreen_layer(layout);
     app.run();
     Ok(())
 }
@@ -48,10 +49,31 @@ fn load_files(folder: Option<String>) -> Result<Vec<String>, io::Error> {
     Ok(list)
 }
 
-fn create_app() -> cursive::CursiveRunnable {
+fn create_app() -> CursiveRunnable {
     let mut siv = cursive::default();
     siv.add_global_callback('q', |s| s.quit());
     siv.set_theme(cursive::theme::Theme::terminal_default());
 
     siv
+}
+
+fn create_list_empty(title: &str) -> ResizedView<Dialog> {
+    Dialog::around(ScrollView::new(DummyView))
+        .title(title)
+        .full_screen()
+}
+
+fn create_list_items(title: &str, contents: Vec<String>) -> ResizedView<Dialog> {
+    let select = SelectView::new()
+        .with_inactive_highlight(false)
+        .with_all_str(contents)
+        .on_submit(select_item);
+
+    Dialog::around(ScrollView::new(select))
+        .title(title)
+        .full_screen()
+}
+
+fn select_item(siv: &mut Cursive, choice: &String) {
+    siv.quit();
 }
